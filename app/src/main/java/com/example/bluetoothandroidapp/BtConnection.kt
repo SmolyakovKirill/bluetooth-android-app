e3f16ed0c6b8541a85f9d50e70a7833142c498c6
SmolyakovKirill <smolyakov.kirusha@gmail.com>
2022-11-30 19:44:59 +0600

    add under control BtConnection

--- app/src/main/java/com/example/bluetoothandroidapp/BtConnection.kt
+++ app/src/main/java/com/example/bluetoothandroidapp/BtConnection.kt
@@ -3,12 +3,14 @@ package com.example.bluetoothandroidapp
 import android.bluetooth.BluetoothAdapter
 
 class BtConnection(private val adapter: BluetoothAdapter) {
+    lateinit var cThread: ConnectThread
 
     fun connect(mac: String) {
         if (adapter.isEnabled && mac.isNotEmpty()) {
             val device = adapter.getRemoteDevice(mac)
             device.let {
-
+                cThread = ConnectThread(it)
+                cThread.start()
             }
         }
     }
