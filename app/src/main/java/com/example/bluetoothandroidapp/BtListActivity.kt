610c99c291dbbe02c888d0d8b16116a30312fb17
SmolyakovKirill <smolyakov.kirusha@gmail.com>
2022-11-30 18:55:39 +0600

    add under control refactoring androidManifest

---
+++ app/src/main/java/com/example/bluetoothandroidapp/BtListActivity.kt
@@ -0,0 +1,45 @@
+package com.example.bluetoothandroidapp
+
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.core.content.getSystemService
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.bluetoothandroidapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private var btAdapter: BluetoothAdapter? = null
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: RcAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        init()
+    }
+
+    private fun init(){
+        val btManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        btAdapter = btManager.adapter
+        adapter = RcAdapter()
+        binding.rcView.layoutManager = LinearLayoutManager(this)
+        binding.rcView.adapter = adapter
+        getPairedDevices()
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getPairedDevices(){
+        val pairedDevices: Set<BluetoothDevice>? = btAdapter?.bondedDevices
+        val tempList = ArrayList<ListItem>()
+        pairedDevices?.forEach{
+            tempList.add(ListItem(it?.name!!,it.address))
+        }
+        adapter.submitList(tempList)
+    }
+}
---
+++ app/src/main/java/com/example/bluetoothandroidapp/ListItem.kt
@@ -0,0 +1,3 @@
+package com.example.bluetoothandroidapp
+
+data class ListItem()
