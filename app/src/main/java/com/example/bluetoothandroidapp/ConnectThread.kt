c5f415bd931e9bb711c9b0f26beaf4e464f9ce7e
SmolyakovKirill <smolyakov.kirusha@gmail.com>
2022-11-30 19:45:34 +0600

    add under control fix ConnectThread

--- app/src/main/java/com/example/bluetoothandroidapp/ConnectThread.kt
+++ app/src/main/java/com/example/bluetoothandroidapp/ConnectThread.kt
@@ -1,4 +1,40 @@
 package com.example.bluetoothandroidapp
 
-class ConnectThread {
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothSocket
+import android.util.Log
+import java.io.IOException
+import java.util.*
+
+@SuppressLint("MissingPermission")
+class ConnectThread(private val device: BluetoothDevice) : Thread() {
+    val uuid = "0000101-0000-1000-8000-00805F9B34FB"
+    var mySocket: BluetoothSocket? = null
+
+    init {
+        try {
+            mySocket = device.createRfcommSocketToServiceRecord(UUID.fromString(uuid))
+        }catch (i: IOException){
+
+        }
+    }
+
+    override fun run(){
+        try {
+            mySocket?.connect()
+        }catch (i: IOException){
+            Log.d("MyLog", "Can not connect to device")
+            closeConnection()
+        }
+    }
+
+    fun closeConnection(){
+        try {
+            mySocket?.connect()
+        }
+        catch (i: IOException){
+
+        }
+    }
 }
