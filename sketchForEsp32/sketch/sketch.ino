1915d7cd2e56baf922407fc82c21459dd957a9d1
SmolyakovKirill <smolyakov.kirusha@gmail.com>
2022-12-04 14:42:34 +0600

    add under control new songs

--- sketchForEsp32/sketch/sketch.ino
+++ sketchForEsp32/sketch/sketch.ino
@@ -10,27 +10,157 @@
 #define BUZZER_PIN 16
 #define BUZZER_CHANNEL 0
 
-int melody[] = {
+enum songs
+{
+  starWars,
+  tetris,
+  gameOfThrones,
+  harryPotter,
+  nokia,
+  startrekintro,
+  takeOnMe
+};
+
+int melodyTakeOnMe[] = {
   
-  NOTE_AS4,8, NOTE_AS4,8, NOTE_AS4,8,//1
-  NOTE_F5,2, NOTE_C6,2,
-  NOTE_AS5,8, NOTE_A5,8, NOTE_G5,8, NOTE_F6,2, NOTE_C6,4,  
-  NOTE_AS5,8, NOTE_A5,8, NOTE_G5,8, NOTE_F6,2, NOTE_C6,4,  
-  NOTE_AS5,8, NOTE_A5,8, NOTE_AS5,8, NOTE_G5,2, NOTE_C5,8, NOTE_C5,8, NOTE_C5,8,
-  NOTE_F5,2, NOTE_C6,2,
-  NOTE_AS5,8, NOTE_A5,8, NOTE_G5,8, NOTE_F6,2, NOTE_C6,4,  
+  NOTE_FS5,8, NOTE_FS5,8,NOTE_D5,8, NOTE_B4,8, REST,8, NOTE_B4,8, REST,8, NOTE_E5,8, 
+  REST,8, NOTE_E5,8, REST,8, NOTE_E5,8, NOTE_GS5,8, NOTE_GS5,8, NOTE_A5,8, NOTE_B5,8,
+  NOTE_A5,8, NOTE_A5,8, NOTE_A5,8, NOTE_E5,8, REST,8, NOTE_D5,8, REST,8, NOTE_FS5,8, 
+  REST,8, NOTE_FS5,8, REST,8, NOTE_FS5,8, NOTE_E5,8, NOTE_E5,8, NOTE_FS5,8, NOTE_E5,8,
+  NOTE_FS5,8, NOTE_FS5,8,NOTE_D5,8, NOTE_B4,8, REST,8, NOTE_B4,8, REST,8, NOTE_E5,8, 
   
-  NOTE_AS5,8, NOTE_A5,8, NOTE_G5,8, NOTE_F6,2, NOTE_C6,4, //8  
-  NOTE_AS5,8, NOTE_A5,8, NOTE_AS5,8, NOTE_G5,2, NOTE_C5,-8, NOTE_C5,16, 
-  NOTE_D5,-4, NOTE_D5,8, NOTE_AS5,8, NOTE_A5,8, NOTE_G5,8, NOTE_F5,8,
-  NOTE_F5,8, NOTE_G5,8, NOTE_A5,8, NOTE_G5,4, NOTE_D5,8, NOTE_E5,4,NOTE_C5,-8, NOTE_C5,16,
-  NOTE_D5,-4, NOTE_D5,8, NOTE_AS5,8, NOTE_A5,8, NOTE_G5,8, NOTE_F5,8,
+  REST,8, NOTE_E5,8, REST,8, NOTE_E5,8, NOTE_GS5,8, NOTE_GS5,8, NOTE_A5,8, NOTE_B5,8,
+  NOTE_A5,8, NOTE_A5,8, NOTE_A5,8, NOTE_E5,8, REST,8, NOTE_D5,8, REST,8, NOTE_FS5,8, 
+  REST,8, NOTE_FS5,8, REST,8, NOTE_FS5,8, NOTE_E5,8, NOTE_E5,8, NOTE_FS5,8, NOTE_E5,8,
+  NOTE_FS5,8, NOTE_FS5,8,NOTE_D5,8, NOTE_B4,8, REST,8, NOTE_B4,8, REST,8, NOTE_E5,8, 
+  REST,8, NOTE_E5,8, REST,8, NOTE_E5,8, NOTE_GS5,8, NOTE_GS5,8, NOTE_A5,8, NOTE_B5,8,
   
-  NOTE_C6,-8, NOTE_G5,16, NOTE_G5,2, REST,8, NOTE_C5,8,//13
-  NOTE_D5,-4, NOTE_D5,8, NOTE_AS5,8, NOTE_A5,8, NOTE_G5,8, NOTE_F5,8,
-  NOTE_F5,8, NOTE_G5,8, NOTE_A5,8, NOTE_G5,4, NOTE_D5,8, NOTE_E5,4,NOTE_C6,-8, NOTE_C6,16,
-  NOTE_F6,4, NOTE_DS6,8, NOTE_CS6,4, NOTE_C6,8, NOTE_AS5,4, NOTE_GS5,8, NOTE_G5,4, NOTE_F5,8,
-  NOTE_C6,1
+  NOTE_A5,8, NOTE_A5,8, NOTE_A5,8, NOTE_E5,8, REST,8, NOTE_D5,8, REST,8, NOTE_FS5,8, 
+  REST,8, NOTE_FS5,8, REST,8, NOTE_FS5,8, NOTE_E5,8, NOTE_E5,8, NOTE_FS5,8, NOTE_E5,8,
+  
+  };
+
+int melodyTetris[] = {
+  
+  NOTE_E5, 4,  NOTE_B4,8,  NOTE_C5,8,  NOTE_D5,4,  NOTE_C5,8,  NOTE_B4,8,
+  NOTE_A4, 4,  NOTE_A4,8,  NOTE_C5,8,  NOTE_E5,4,  NOTE_D5,8,  NOTE_C5,8,
+  NOTE_B4, -4,  NOTE_C5,8,  NOTE_D5,4,  NOTE_E5,4,
+  NOTE_C5, 4,  NOTE_A4,4,  NOTE_A4,8,  NOTE_A4,4,  NOTE_B4,8,  NOTE_C5,8,
+
+  NOTE_D5, -4,  NOTE_F5,8,  NOTE_A5,4,  NOTE_G5,8,  NOTE_F5,8,
+  NOTE_E5, -4,  NOTE_C5,8,  NOTE_E5,4,  NOTE_D5,8,  NOTE_C5,8,
+  NOTE_B4, 4,  NOTE_B4,8,  NOTE_C5,8,  NOTE_D5,4,  NOTE_E5,4,
+  NOTE_C5, 4,  NOTE_A4,4,  NOTE_A4,4, REST, 4,
+
+  NOTE_E5, 4,  NOTE_B4,8,  NOTE_C5,8,  NOTE_D5,4,  NOTE_C5,8,  NOTE_B4,8,
+  NOTE_A4, 4,  NOTE_A4,8,  NOTE_C5,8,  NOTE_E5,4,  NOTE_D5,8,  NOTE_C5,8,
+  NOTE_B4, -4,  NOTE_C5,8,  NOTE_D5,4,  NOTE_E5,4,
+  NOTE_C5, 4,  NOTE_A4,4,  NOTE_A4,8,  NOTE_A4,4,  NOTE_B4,8,  NOTE_C5,8,
+
+  NOTE_D5, -4,  NOTE_F5,8,  NOTE_A5,4,  NOTE_G5,8,  NOTE_F5,8,
+  NOTE_E5, -4,  NOTE_C5,8,  NOTE_E5,4,  NOTE_D5,8,  NOTE_C5,8,
+  NOTE_B4, 4,  NOTE_B4,8,  NOTE_C5,8,  NOTE_D5,4,  NOTE_E5,4,
+  NOTE_C5, 4,  NOTE_A4,4,  NOTE_A4,4, REST, 4,
+  
+
+  NOTE_E5,2,  NOTE_C5,2,
+  NOTE_D5,2,   NOTE_B4,2,
+  NOTE_C5,2,   NOTE_A4,2,
+  NOTE_GS4,2,  NOTE_B4,4,  REST,8, 
+  NOTE_E5,2,   NOTE_C5,2,
+  NOTE_D5,2,   NOTE_B4,2,
+  NOTE_C5,4,   NOTE_E5,4,  NOTE_A5,2,
+  NOTE_GS5,2,
+  
+};
+
+int melodyGameOfThrones[] = {
+
+  NOTE_G4,8, NOTE_C4,8, NOTE_DS4,16, NOTE_F4,16, NOTE_G4,8, NOTE_C4,8, NOTE_DS4,16, NOTE_F4,16, //1
+  NOTE_G4,8, NOTE_C4,8, NOTE_DS4,16, NOTE_F4,16, NOTE_G4,8, NOTE_C4,8, NOTE_DS4,16, NOTE_F4,16,
+  NOTE_G4,8, NOTE_C4,8, NOTE_E4,16, NOTE_F4,16, NOTE_G4,8, NOTE_C4,8, NOTE_E4,16, NOTE_F4,16,
+  NOTE_G4,8, NOTE_C4,8, NOTE_E4,16, NOTE_F4,16, NOTE_G4,8, NOTE_C4,8, NOTE_E4,16, NOTE_F4,16,
+  NOTE_G4,-4, NOTE_C4,-4,//5
+
+  NOTE_DS4,16, NOTE_F4,16, NOTE_G4,4, NOTE_C4,4, NOTE_DS4,16, NOTE_F4,16, //6
+  NOTE_D4,-1, //7 and 8
+  NOTE_F4,-4, NOTE_AS3,-4,
+  NOTE_DS4,16, NOTE_D4,16, NOTE_F4,4, NOTE_AS3,-4,
+  NOTE_DS4,16, NOTE_D4,16, NOTE_C4,-1, //11 and 12
+
+  //repeats from 5
+  NOTE_G4,-4, NOTE_C4,-4,//5
+
+  NOTE_DS4,16, NOTE_F4,16, NOTE_G4,4, NOTE_C4,4, NOTE_DS4,16, NOTE_F4,16, //6
+  NOTE_D4,-1, //7 and 8
+  NOTE_F4,-4, NOTE_AS3,-4,
+  NOTE_DS4,16, NOTE_D4,16, NOTE_F4,4, NOTE_AS3,-4,
+  NOTE_DS4,16, NOTE_D4,16, NOTE_C4,-1, //11 and 12
+  NOTE_G4,-4, NOTE_C4,-4,
+  NOTE_DS4,16, NOTE_F4,16, NOTE_G4,4,  NOTE_C4,4, NOTE_DS4,16, NOTE_F4,16,
+
+  NOTE_D4,-2,//15
+  NOTE_F4,-4, NOTE_AS3,-4,
+  NOTE_D4,-8, NOTE_DS4,-8, NOTE_D4,-8, NOTE_AS3,-8,
+  NOTE_C4,-1,
+  NOTE_C5,-2,
+  NOTE_AS4,-2,
+  NOTE_C4,-2,
+  NOTE_G4,-2,
+  NOTE_DS4,-2,
+  NOTE_DS4,-4, NOTE_F4,-4, 
+  NOTE_G4,-1,
+  
+  NOTE_C5,-2,//28
+  NOTE_AS4,-2,
+  NOTE_C4,-2,
+  NOTE_G4,-2, 
+  NOTE_DS4,-2,
+  NOTE_DS4,-4, NOTE_D4,-4,
+  NOTE_C5,8, NOTE_G4,8, NOTE_GS4,16, NOTE_AS4,16, NOTE_C5,8, NOTE_G4,8, NOTE_GS4,16, NOTE_AS4,16,
+  NOTE_C5,8, NOTE_G4,8, NOTE_GS4,16, NOTE_AS4,16, NOTE_C5,8, NOTE_G4,8, NOTE_GS4,16, NOTE_AS4,16,
+  
+  REST,4, NOTE_GS5,16, NOTE_AS5,16, NOTE_C6,8, NOTE_G5,8, NOTE_GS5,16, NOTE_AS5,16,
+  NOTE_C6,8, NOTE_G5,16, NOTE_GS5,16, NOTE_AS5,16, NOTE_C6,8, NOTE_G5,8, NOTE_GS5,16, NOTE_AS5,16,  
+};
+
+int melodyHarryPotter[] = {
+
+  REST, 2, NOTE_D4, 4,
+  NOTE_G4, -4, NOTE_AS4, 8, NOTE_A4, 4,
+  NOTE_G4, 2, NOTE_D5, 4,
+  NOTE_C5, -2, 
+  NOTE_A4, -2,
+  NOTE_G4, -4, NOTE_AS4, 8, NOTE_A4, 4,
+  NOTE_F4, 2, NOTE_GS4, 4,
+  NOTE_D4, -1, 
+  NOTE_D4, 4,
+
+  NOTE_G4, -4, NOTE_AS4, 8, NOTE_A4, 4, //10
+  NOTE_G4, 2, NOTE_D5, 4,
+  NOTE_F5, 2, NOTE_E5, 4,
+  NOTE_DS5, 2, NOTE_B4, 4,
+  NOTE_DS5, -4, NOTE_D5, 8, NOTE_CS5, 4,
+  NOTE_CS4, 2, NOTE_B4, 4,
+  NOTE_G4, -1,
+  NOTE_AS4, 4,
+     
+  NOTE_D5, 2, NOTE_AS4, 4,//18
+  NOTE_D5, 2, NOTE_AS4, 4,
+  NOTE_DS5, 2, NOTE_D5, 4,
+  NOTE_CS5, 2, NOTE_A4, 4,
+  NOTE_AS4, -4, NOTE_D5, 8, NOTE_CS5, 4,
+  NOTE_CS4, 2, NOTE_D4, 4,
+  NOTE_D5, -1, 
+  REST,4, NOTE_AS4,4,  
+
+  NOTE_D5, 2, NOTE_AS4, 4,//26
+  NOTE_D5, 2, NOTE_AS4, 4,
+  NOTE_F5, 2, NOTE_E5, 4,
+  NOTE_DS5, 2, NOTE_B4, 4,
+  NOTE_DS5, -4, NOTE_D5, 8, NOTE_CS5, 4,
+  NOTE_CS4, 2, NOTE_AS4, 4,
+  NOTE_G4, -1, 
   
 };
 
@@ -40,8 +170,7 @@ const int yellowLedPin = 14;
 
 BluetoothSerial SerialBT;
 
-int tempo = 220;
-int notes = sizeof(melody) / sizeof(melody[0]) / 2;
+int tempo = 320;
 int wholenote = (60000 * 4) / tempo;
 int divider = 0, noteDuration = 0;
 
@@ -54,6 +183,70 @@ void setup() {
   Serial.println("The device started, now you can pair it with bluetooth!");
 }
 
+void playTetrisMusic(){
+    int notes = sizeof(melodyTetris) / sizeof(melodyTetris[0]) / 2;
+    for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
+            divider = melodyTetris[thisNote + 1];
+            if (divider > 0) {
+              noteDuration = (wholenote) / divider;
+            } else if (divider < 0) {
+              noteDuration = (wholenote) / abs(divider);
+              noteDuration *= 1.5; // increases the duration in half for dotted notes
+            }
+            tone(BUZZER_PIN, melodyTetris[thisNote], noteDuration*0.9, BUZZER_CHANNEL);
+            delay(noteDuration);
+            noTone(BUZZER_PIN, BUZZER_CHANNEL);
+        }
+}
+
+void playTakeOnMeMusic(){
+    int notes = sizeof(melodyTakeOnMe) / sizeof(melodyTakeOnMe[0]) / 2;
+    for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
+            divider = melodyTakeOnMe[thisNote + 1];
+            if (divider > 0) {
+              noteDuration = (wholenote) / divider;
+            } else if (divider < 0) {
+              noteDuration = (wholenote) / abs(divider);
+              noteDuration *= 1.5; // increases the duration in half for dotted notes
+            }
+            tone(BUZZER_PIN, melodyTakeOnMe[thisNote], noteDuration*0.9, BUZZER_CHANNEL);
+            delay(noteDuration);
+            noTone(BUZZER_PIN, BUZZER_CHANNEL);
+        }
+}
+
+void playGameOfThronesMusic(){
+    int notes = sizeof(melodyGameOfThrones) / sizeof(melodyGameOfThrones[0]) / 2;
+    for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
+            divider = melodyGameOfThrones[thisNote + 1];
+            if (divider > 0) {
+              noteDuration = (wholenote) / divider;
+            } else if (divider < 0) {
+              noteDuration = (wholenote) / abs(divider);
+              noteDuration *= 1.5; // increases the duration in half for dotted notes
+            }
+            tone(BUZZER_PIN, melodyGameOfThrones[thisNote], noteDuration*0.9, BUZZER_CHANNEL);
+            delay(noteDuration);
+            noTone(BUZZER_PIN, BUZZER_CHANNEL);
+        }
+}
+
+void playHarryPotterMusic(){
+    int notes = sizeof(melodyHarryPotter) / sizeof(melodyHarryPotter[0]) / 2;
+    for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
+            divider = melodyHarryPotter[thisNote + 1];
+            if (divider > 0) {
+              noteDuration = (wholenote) / divider;
+            } else if (divider < 0) {
+              noteDuration = (wholenote) / abs(divider);
+              noteDuration *= 1.5; // increases the duration in half for dotted notes
+            }
+            tone(BUZZER_PIN, melodyHarryPotter[thisNote], noteDuration*0.9, BUZZER_CHANNEL);
+            delay(noteDuration);
+            noTone(BUZZER_PIN, BUZZER_CHANNEL);
+        }
+}
+
 void loop() {
   if (Serial.available()) {
     SerialBT.write(Serial.read());
@@ -62,27 +255,19 @@ void loop() {
     char ch = SerialBT.read();
     Serial.write(SerialBT.read());
     if(ch == 'A'){
-         for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
-            divider = melody[thisNote + 1];
-            if (divider > 0) {
-              noteDuration = (wholenote) / divider;
-            } else if (divider < 0) {
-              noteDuration = (wholenote) / abs(divider);
-              noteDuration *= 1.5; // increases the duration in half for dotted notes
-            }
-            tone(BUZZER_PIN, melody[thisNote], noteDuration*0.9, BUZZER_CHANNEL);
-            delay(noteDuration);
-            noTone(BUZZER_PIN, BUZZER_CHANNEL);
-        }
+        playTetrisMusic();
         digitalWrite (redLedPin, HIGH);
       }
     if(ch == 'a'){
+        playGameOfThronesMusic();
         digitalWrite (redLedPin, LOW);
       }
     if(ch == 'B'){
+        playTakeOnMeMusic();
         digitalWrite (greenLedPin, HIGH);
      }
     if(ch == 'b'){
+        playHarryPotterMusic();
         digitalWrite (greenLedPin, LOW);
      }
   }
